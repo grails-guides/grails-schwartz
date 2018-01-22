9cf7da6cfbe3f239722d58c6c1567187e0260f8e
Iván López <lopez.ivan@gmail.com>
2018-01-22 10:28:47 +0100

    Logic in service and concurrent execution

--- complete/grails-app/services/demo/DailyEmailJobService.groovy
+++ complete/grails-app/services/demo/DailyEmailJobService.groovy
@@ -6,26 +6,21 @@ import groovy.util.logging.Slf4j
 import org.quartz.JobExecutionContext
 import org.quartz.JobExecutionException
 
+//tag::dailyEmailJobService[]
 @CompileStatic
 @Slf4j
 class DailyEmailJobService implements SchwartzJob {
 
-    EmailService emailService
+    EmailService emailService // <1>
 
     void execute(JobExecutionContext context) throws JobExecutionException {
-        emailService.send('john.doe@example.com')
+        emailService.send('john.doe@example.com') // <2>
     }
 
     void buildTriggers() {
-
-//        triggers << factory('Daily email job')
-//            .startAt(DateBuilder.todayAt(4, 30, 0))
-//            .intervalInDays(1)
-//            .misfireHandling(MisfireHandling.IgnoreMisfires)
-//            .build()
-
         triggers << factory('Daily email job')
-            .cronSchedule("0 30 4 1/1 * ? *")
+            .cronSchedule("0 30 4 1/1 * ? *") // <3>
             .build()
     }
 }
+//end::dailyEmailJobService[]
