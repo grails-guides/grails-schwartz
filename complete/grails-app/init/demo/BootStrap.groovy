b478af91798b642fc45a57400bff5ea31e9b583f
Sergio del Amo <sergio.delamo@softamo.com>
2018-01-23 08:36:48 +0100

    Trigger Manually example

--- complete/grails-app/init/demo/BootStrap.groovy
+++ complete/grails-app/init/demo/BootStrap.groovy
@@ -5,7 +5,12 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class BootStrap {
 
+	RegisterService registerService
+
     def init = { servletContext ->
+    	registerService.register('harry@howards.com')
+    	sleep(20_000)
+    	registerService.register('ron@howards.com')
     }
     def destroy = {
     }
